dec5f2949bc9b28ae80013cddbc0152ef811d4f8
Chin Hui <chhew96@gmail.com>
2019-10-29 21:28:56 +0800

    Create CameraFocus.cs

---
+++ CameraFocus.cs
@@ -0,0 +1,27 @@
+//Code to set camera auto focus for best view
+
+public class CameraFocus : MonoBehaviour
+{
+    void Start()
+    {
+        var vuforia = VuforiaARController.Instance;
+        vuforia.RegisterVuforiaStartedCallback(OnVuforiaStarted);
+        vuforia.RegisterOnPauseCallback(OnPaused);
+    }
+
+    private void OnVuforiaStarted()
+    {
+        CameraDevice.Instance.SetFocusMode(
+            CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+    }
+
+    private void OnPaused(bool paused)
+    {
+        if (!paused) // resumed
+        {
+            // Set again autofocus mode when app is resumed
+            CameraDevice.Instance.SetFocusMode(
+               CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+        }
+    }
+}
